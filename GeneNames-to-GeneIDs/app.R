6281e43ba71582ee144fc3d9d0fcd3549e7026ab
aartrama <aartrama@umail.iu.edu>
2018-06-10 13:48:32 -0400

    Automatic detection of input

--- GeneNames-to-GeneIDs/app.R
+++ GeneNames-to-GeneIDs/app.R
@@ -15,23 +15,18 @@ ui <- fluidPage(
                                 "Human" = 2,
                                 "Rat" = 3),
                    selected = 1),
-      radioButtons("radio_function", label=h3("What would you like to do?"),
-                   choices=list("Convert gene ID to gene name" = 1,
-                                "Convert gene name to gene ID" = 2
-                                ),
-                   selected = 1),
-      fileInput("file2", h3("Please upload a gene list"), 
+      fileInput("file2", h3("Please upload a list of ENSEMBL gene IDs/gene names"), 
                 accept = c("text/csv", "text/comma-separated-values, 
                            text/plain", ".csv")),
       submitButton("Submit"),
       hr(),
-      downloadButton("downloadData", "Download")
+      downloadButton("downloadData", "Export table")
                 ),
     
     mainPanel(
       tableOutput("output_geneids")
     )
-)
+  )
 )
 
 server <- function(input, output) {
@@ -42,43 +37,39 @@ server <- function(input, output) {
     }
     
     # read input genes
-    data2 <- data.frame(read.table(file2$datapath, stringsAsFactors = F))
+    data1 <- data.frame(read.table(file2$datapath, stringsAsFactors = F))
     
     # determine species
     if (input$radio == 1) {
       dataset_name <- "mmusculus_gene_ensembl"
     } else if (input$radio == 2) {
       dataset_name <- "hsapiens_gene_ensembl"
-      data2 <- data.frame(lapply(data2, function(v) {
+      data1 <- data.frame(lapply(data1, function(v) {
         if (is.character(v)) return(toupper(v))
         else return(v)
       }))
     } else if (input$radio == 3) {
-        dataset_name <- "rnorvegicus_gene_ensembl"
+      dataset_name <- "rnorvegicus_gene_ensembl"
     }
-
-    # gene id or gene name
-    if (input$radio_function == 1) {
-      common_col <- "ensembl_gene_id"
-      colnames(data2) <- c(common_col)
-      
-    } else if (input$radio_function == 2) {
-      common_col <- "external_gene_name"
-      colnames(data2) <- c(common_col)
+    
+    if(grepl("ENS", data1[1,1])){
+      common_col = "ensembl_gene_id"
+    } else {
+      common_col = "external_gene_name"
     }
+    
+    colnames(data1) <- c(common_col) 
     
     ensembl <- useMart("ensembl", host="http://aug2017.archive.ensembl.org", 
                        dataset = dataset_name)
     mapping <- getBM(attributes = c('ensembl_gene_id', 'external_gene_name', 
                                     'chromosome_name', 'start_position', 
                                     'end_position', 'gene_biotype', 'description'), 
-                     values = data2, mart = ensembl)
+                     values = data1, mart = ensembl)
     
-
-    
-    fileText <- left_join(x=data2, y=mapping, 
+    fileText <- left_join(x=data1, y=mapping, 
                           by=common_col)
-  
+    
   })
   
   output$output_geneids <- renderTable({
